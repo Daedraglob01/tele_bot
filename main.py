65fcf55940c68c4f80b4bf9e349087c45497bbbf
Didkovskyi Davyd <116342774+Daedraglob01@users.noreply.github.com>
2024-02-15 15:40:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot('6770420064:AAHrpItQGicrYuVAqAvLKgnrWnTfeCWtdXw')
+API = '1bc9818d4d78ee8300248f8fdceb004a'
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привіт, радий тебе вітати! Напиши назву міста погода якого тобі цікава.')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        bot.reply_to(message, f'Зараз погода {data['main']['temp']} градусів цельсія')
+    else:
+        bot.reply_to(message, 'Неправильно вказане місто')
+
+
+bot.polling(none_stop=True)
